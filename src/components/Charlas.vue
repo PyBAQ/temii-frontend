5dfb0c28a8771ca1d03cc10288a2fb72f3f455aa
jdash99 <jdash99@gmail.com>
2019-03-31 22:00:01 -0500

    Base en Vue para el proyecto frontend de Temii

---
+++ src/components/Charlas.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="header">
+    <div class="header-logo">Temii</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Charlas',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+body {
+  padding: 0px;
+  margin: 0px;
+}
+
+.header {
+  margin: 0px;
+  height: 80px;
+  border-bottom: 1px solid #e5e5e5;
+}
+
+.header-logo {
+  font-family: 'Pacifico';
+  font-size: 48px;
+  color: #00a7e1;
+  padding-left: 16px;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Charlas/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Charlas from '@/components/Charlas.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Charlas
+  }
+}
+</script>
